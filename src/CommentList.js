95d8cccafe57eedcc770103e844359e30861776d
djay <djay@boegismedia.com>
2022-04-21 10:13:17 +0700

    display comment status

--- src/CommentList.js
+++ src/CommentList.js
@@ -3,7 +3,18 @@ import React from "react";
 const CommentList = ({comments}) => {
 
   const renderedComments = comments.map( comment => {
-    return <li key={comment.id}>{comment.content}</li>
+    let content = comment.content;
+    switch(comment.status) {
+      case 'rejected':
+        content += ' - this comment was rejected';
+        break;
+      case 'pending':
+        content += ' - this comment is pending approval';
+        break;
+      default:
+        break;
+    }
+    return <li key={comment.id}>{content}</li>
   })
   return <div>
     <p><i>{comments.length+` comment${comments.length>1?'s':''}`}</i></p>
